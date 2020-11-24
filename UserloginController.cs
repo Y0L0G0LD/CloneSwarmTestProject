04f8c3e0510eafc8d3f9019aa10cf464d935dce6
Y0L0G0LD <69674356+Y0L0G0LD@users.noreply.github.com>
2020-11-24 20:08:39 +0100

    First Commit
    Commit with Controller Files and a Library File to Test for Code Clones

---
+++ UserloginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcEncrypandDecryp.Models;
+using ClientsideEncryption;
+
+namespace MvcEncrypandDecryp.Controllers
+{
+    public class UserloginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Userlogin objUL )
+        {
+            if (ModelState.IsValid)
+            {
+                var username = AESEncrytDecry.DecryptStringAES(objUL.HDUser);
+                var password = AESEncrytDecry.DecryptStringAES(objUL.HDpass);
+
+                if (username == "keyError" && password == "keyError")
+                {
+                    TempData["notice"] = "Invalid  Login";
+                }
+                else
+                {
+                    TempData["notice"] = "login successfully";
+                }
+                return View(objUL);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid  Login");
+                return View(objUL);
+            }
+        }
+
+    }
+}
